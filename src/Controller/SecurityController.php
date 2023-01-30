de460082785888ffb791f373ae36aed494a3d996
muthuxv <muthulan.m@gmail.com>
2023-01-30 12:17:30 +0100

    hash password for update user and new user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,7 +21,9 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $user[] = $this->getUser();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'user' => $user]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
