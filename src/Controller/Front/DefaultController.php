e608d235d465727cf900b20e306e9c17e89b59b0
muthuxv <muthulan.m@gmail.com>
2023-02-25 22:20:50 +0100

    page liste des heros par rang + top 3 hero

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use App\Repository\HeroRepository;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Form\UpdateUserProfile;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,11 +19,33 @@ use App\Repository\EventRepository;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_index')]
-    public function index(UserRepository $userRepository, UserInterface $user, EventRepository $eventRepository): Response
+    public function index(UserRepository $userRepository, UserInterface $user, EventRepository $eventRepository, HeroRepository $heroRepository): Response
     {
+        $bestHeroes = $heroRepository->findAll();
+
+        //tri des heroes par ordre de rang en commencant par le rang S
+        $ranks = ['S', 'A', 'B', 'C'];
+
+        $bestHeroesByRank = [];
+        foreach ($ranks as $rank) {
+            foreach ($bestHeroes as $hero) {
+                if ($hero->getRank() == $rank) {
+                    $bestHeroesByRank[$rank][] = $hero;
+                }
+            }
+        }
+        //get the best heroes
+        $bestHeroes = [];
+        foreach ($bestHeroesByRank as $rank) {
+            foreach ($rank as $hero) {
+                $bestHeroes[] = $hero;
+            }
+        }
+
         return $this->render('front/default/index.html.twig', [
             'user' => $userRepository->findBy(array('id' => $user->getId())),
             'events' => $eventRepository->findAllByDateAndLimit(),
+            'bestHeroes' => $bestHeroes,
         ]);
     }
 
@@ -54,6 +77,29 @@ class DefaultController extends AbstractController
         return $this->renderForm('front/default/update_user_profile.html.twig', [
             'user' => $userRepository->findBy(array('id' => $user->getId())),
             'form' => $form,
+        ]);
+    }
+
+    //liste des heroes par ordre de rang
+    #[Route('/heroes', name: 'heroes')]
+    public function heroes(HeroRepository $heroRepository, UserInterface $user, UserRepository $userRepository): Response
+    {
+        $heroes = $heroRepository->findAll();
+
+        $heroesByRank = [];
+        foreach ($heroes as $hero) {
+            $rank = $hero->getRank();
+            if (!isset($heroesByRank[$rank])) {
+                $heroesByRank[$rank] = [];
+                $heroesByRank[$rank][] = $hero;
+            }else {
+                $heroesByRank[$rank][] = $hero;
+            }
+        }
+
+        return $this->render('front/default/heroes.html.twig', [
+            'heroes' => $heroesByRank,
+            'user' => $userRepository->findBy(array('id' => $user->getId())),
         ]);
     }
 }
